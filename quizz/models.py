d95aef05434dbbeabd03f5230b40b020061ad7f7
AbdusamadDev <abdusamaddev571@gmail.com>
2023-10-19 00:25:59 +0500

    Initial Commit

---
+++ quizz/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Answers(models.Model):
+    CHOICES = (
+        ("a", "Answer A"),
+        ("b", "Answer B"),
+        ("c", "Answer C"),
+    )
+
+    a = models.CharField(max_length=200)
+    b = models.CharField(max_length=200)
+    c = models.CharField(max_length=200)
+    correct_answer = models.CharField(choices=CHOICES)
+
+
+class Questions(models.Model):
+    text = models.CharField(max_length=500)
+    answers = models.ForeignKey(to=Answers, on_delete=models.CASCADE)
